030d394e8a45091f0b76499434b881dfd0c107e9
Максимов Максим <MAMaksimov.SBT@sberbank.ru>
2023-08-29 18:10:58 +0300

    Initial Commit

---
+++ user.py
@@ -0,0 +1,21 @@
+#{
+#  "id": 0,
+#  "username": "string",
+#  "firstName": "string",
+#  "lastName": "string",
+#  "email": "string",
+#  "password": "string",
+#  "phone": "string",
+#  "userStatus": 0
+#}
+
+
+request = {}
+request["id"] = 0
+request["username"] = "string"
+request["firstName"] = ""
+request["lastName"] = ""
+request["email"] = "string"
+request["password"] = "string"
+request["phone"] = ""
+request["userStatus"] = "0"
