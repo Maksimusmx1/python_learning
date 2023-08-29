5394bff2f90b743b23f28397edfb24afca22a3e5
Максим Максимов <MAMaksimov.SBT@sberbank.ru>
2023-08-29 19:07:31 +0300

    добавил файл store_order

---
+++ store_order.py
@@ -0,0 +1,18 @@
+#{
+#  "id": 0,
+#  "petId": 0,
+#  "quantity": 0,
+#  "shipDate": "2023-08-29T16:01:26.187Z",
+#  "status": "placed",
+#  "complete": true
+#}
+
+
+request = {}
+request["id"] = 0
+request["petId"] = 0
+request["quantity"] = 0
+request["shipDate"] = "2023-08-29T16:01:26.187Z"
+request["status"] = "placed"
+request["complete"] = true
+
